873ad0ceec7439800e3d7ec779b6ba47fc51b81f
Andrei Gavrila <andreigavrila1401@gmail.com>
2020-05-01 06:08:30 +0300

    terrain gen with shader, collider, and a good enough camera to use in the game

---
+++ Assets/Scripts/Terrain/Editor/TerrainGeneratorEditor.cs
@@ -0,0 +1,23 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Terrain {
+    [CustomEditor (typeof (TerrainGenerator))]
+    public class TerrainGeneratorEditor : Editor {
+        TerrainGenerator terrainGen;
+
+        public override void OnInspectorGUI () {
+            DrawDefaultInspector ();
+
+            if (GUILayout.Button ("Refresh")) {
+                terrainGen.Generate ();
+            }
+        }
+
+        void OnEnable () {
+            terrainGen = (TerrainGenerator) target;
+        }
+    }
+}
---
+++ Assets/Scripts/Terrain/HeightmapGenerator.cs
@@ -0,0 +1,54 @@
+﻿﻿using UnityEngine;
+
+namespace Terrain {
+    public static class HeightmapGenerator {
+
+        public static float[, ] GenerateHeightmap (NoiseSettings noiseSettings, int size, bool normalize = true) {
+            var map = new float[size, size];
+            var prng = new System.Random (noiseSettings.seed);
+            var noise = new Noise (noiseSettings.seed);
+
+            // Generate random offset for each layer
+            var offsets = new Vector2[noiseSettings.numLayers];
+            for (var layer = 0; layer < noiseSettings.numLayers; layer++) {
+                offsets[layer] = new Vector2 ((float) prng.NextDouble () * 2 - 1, (float) prng.NextDouble () * 2 - 1) * 10000;
+                // offsets[layer] += noiseSettings.offset;
+            }
+
+            var minHeight = float.MaxValue;
+            var maxHeight = float.MinValue;
+
+            for (var y = 0; y < size; y++) {
+                for (var x = 0; x < size; x++) {
+                    var frequency = noiseSettings.scale;
+                    var amplitude = 1f;
+                    var height = 0f;
+
+                    // Sum layers of noise
+                    for (var layer = 0; layer < noiseSettings.numLayers; layer++) {
+                        var sampleX = (double) x / size * frequency + offsets[layer].x + noiseSettings.offset.x;
+                        var sampleY = (double) y / size * frequency + offsets[layer].y + noiseSettings.offset.y;
+                        height += (float) noise.Evaluate (sampleX, sampleY) * amplitude;
+                        frequency *= noiseSettings.lacunarity;
+                        amplitude *= noiseSettings.persistence;
+                    }
+                    map[x, y] = height;
+                    minHeight = Mathf.Min (minHeight, height);
+                    maxHeight = Mathf.Max (maxHeight, height);
+                }
+            }
+
+            // Normalize values to range 0-1
+            if (normalize) {
+                for (var y = 0; y < size; y++) {
+                    for (var x = 0; x < size; x++) {
+                        map[x, y] = Mathf.InverseLerp (minHeight, maxHeight, map[x, y]);
+                    }
+                }
+            }
+
+            return map;
+        }
+
+    }
+}
